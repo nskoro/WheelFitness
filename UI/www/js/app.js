193923e856c4be5d1d603a12a0028639504e587d
Neven Skoro <nskoro@xiim.com>
2017-03-14 23:59:10 -0600

    Added initial pages and navigation

--- UI/www/js/app.js
+++ UI/www/js/app.js
@@ -4,7 +4,7 @@
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
 // 'starter.controllers' is found in controllers.js
-angular.module('starter', ['ionic', 'starter.controllers'])
+angular.module('starter', ['ionic', 'fitness.splash','fitness.home','fitness.login','fitness.menu', 'fitness.settings', 'fitness.rankings'])
 
 .run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
@@ -26,48 +26,52 @@ angular.module('starter', ['ionic', 'starter.controllers'])
   $stateProvider
 
     .state('app', {
-    url: '/app',
-    abstract: true,
-    templateUrl: 'templates/menu.html',
-    controller: 'AppCtrl'
+      url: '/app',
+      abstract: true,
+      templateUrl: 'pages/menu/menu.html',
+      controller: 'AppCtrl as appCtrl'
   })
-
-  .state('app.search', {
-    url: '/search',
+  .state('splash', {
+      url: '/splash',
+      templateUrl: 'pages/splash/splash.html',
+      controller: 'SplashCtrl as splashCtrl'
+    })
+  .state('app.home', {
+    url: '/home',
     views: {
       'menuContent': {
-        templateUrl: 'templates/search.html'
+        templateUrl: 'pages/home/home.html',
+        controller: 'HomeCtrl as homeCtrl'
       }
     }
   })
-
-  .state('app.browse', {
-      url: '/browse',
-      views: {
-        'menuContent': {
-          templateUrl: 'templates/browse.html'
-        }
-      }
-    })
-    .state('app.playlists', {
-      url: '/playlists',
-      views: {
-        'menuContent': {
-          templateUrl: 'templates/playlists.html',
-          controller: 'PlaylistsCtrl'
-        }
-      }
-    })
-
-  .state('app.single', {
-    url: '/playlists/:playlistId',
+   .state('app.login', {
+    url: '/login',
     views: {
       'menuContent': {
-        templateUrl: 'templates/playlist.html',
-        controller: 'PlaylistCtrl'
+        templateUrl: 'pages/login/login.html',
+        controller: 'LoginCtrl as loginCtrl'
       }
     }
-  });
+  })
+  .state('app.settings', {
+      url: '/settings',
+      views: {
+        'menuContent': {
+          templateUrl: 'pages/settings/settings.html',
+          controller: 'SettingsCtrl as settingsCtrl'
+        }
+      }
+    })
+    .state('app.rankings', {
+      url: '/rankings',
+      views: {
+        'menuContent': {
+          templateUrl: 'pages/rankings/rankings.html',
+          controller: 'RankCtrl as rankCtrl'
+        }
+      }
+    });
   // if none of the above states are matched, use this as the fallback
-  $urlRouterProvider.otherwise('/app/playlists');
+  $urlRouterProvider.otherwise('/splash');
 });
