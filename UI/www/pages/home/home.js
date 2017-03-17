84d0afb1c5ae97bf94e2e2510898e481ec29b37b
Neven Skoro <nskoro@xiim.com>
2017-03-17 14:08:26 -0600

    added new image

--- UI/www/pages/home/home.js
+++ UI/www/pages/home/home.js
@@ -4,13 +4,6 @@ angular.module('fitness.home', [])
 
     var self = this ;
 
-    self.name = "Bret";
-    
-    self.foo = function() {
-
-    }
-
-
   // With the new view caching in Ionic, Controllers are only called
   // when they are recreated or on app start, instead of every page change.
   // To listen for when this page is active (for example, to refresh data),
