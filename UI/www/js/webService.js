51b321bd52ec892bda4acd0e02da4747505552ef
Neven Skoro <nskoro@xiim.com>
2017-03-17 15:11:17 -0600

    added phrases

--- UI/www/js/webService.js
+++ UI/www/js/webService.js
@@ -2,5 +2,13 @@
 app.service('webService', function($http, $ionicModal, $interval, $timeout, $state, $q, $window) {
 
  // put all the fitbit http requests here
- 
+  this.phrases = [{ id: '1',
+                    category: 'nature',
+                    phrase: 'here goes the phrase',
+                    difficulty: '1' },
+                  { id: '2',
+                    category: 'nature',
+                    phrase: 'here goes another phrase',
+                    difficulty: '5' }, // out of 10
+                 ];
 })
