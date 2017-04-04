75dd276680a0f347d8e93570c40fc340d275267b
Neven Skoro <nskoro@xiim.com>
2017-04-04 11:27:36 -0600

    added logout to settings

--- UI/www/pages/settings/settings.js
+++ UI/www/pages/settings/settings.js
@@ -1,13 +1,16 @@
 angular.module('fitness.settings', [])
 
-.controller('SettingsCtrl', function($scope, $ionicModal, $timeout) {
+.controller('SettingsCtrl', function($scope, gameService, $ionicModal, $timeout) {
 
   // With the new view caching in Ionic, Controllers are only called
   // when they are recreated or on app start, instead of every page change.
   // To listen for when this page is active (for example, to refresh data),
   // listen for the $ionicView.enter event:
-  //$scope.$on('$ionicView.enter', function(e) {
-  //});
+  $scope.$on('$ionicView.enter', function(e) {
+  
+    gameService.getFitbitData();
+  });
 
+  this.logout = gameService.logout ;
   
 })
